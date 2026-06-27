9b7b1c5f55857285955bcb80b7e5c998787dc3a5
Just-Nova23 <vlad@novaxhosting.com>
2026-06-28 00:10:55 +0200

    stub legacy Android version headers

---
+++ engine/bridge/include/android_version.h
@@ -0,0 +1,18 @@
+/* SPDX-License-Identifier: LGPL-2.1-or-later */
+#pragma once
+
+#ifndef VERSION_MAJOR
+#define VERSION_MAJOR 0
+#endif
+
+#ifndef VERSION_MINOR
+#define VERSION_MINOR 0
+#endif
+
+#ifndef VERSION_PATCH
+#define VERSION_PATCH 0
+#endif
+
+#ifndef VERSION_STRING
+#define VERSION_STRING "LuaNet-Android"
+#endif
